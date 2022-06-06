8940277c2e73b4de971f2f14db0a9398907d62c9
PeterBroom <harleybroom@googlemail.com>
2022-06-06 10:00:45 +0100

    product variant price change

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -98,11 +98,10 @@ export default function ProductForm({ product }) {
     }
   }, [productInventory, selectedVariant])
 
-
   return (
     <div className="rounded-2xl p-4 shadow-lg flex flex-col w-full md:w-1/3">
       <h2 className="text-2xl font-bold">{product.title}</h2>
-      <span className="pb-3">{formatter.format(product.variants.edges[0].node.priceV2.amount)}</span>
+      <span className="pb-3">{formatter.format(selectedVariant.variantPrice)}</span>
       {
         product.options[0].values[0] !== 'Default Title' &&
         product.options.map(({ name, values }) => (
