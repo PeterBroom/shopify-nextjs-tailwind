e74b145969be46c0ab6f930da412b734176b64a9
PeterBroom <harleybroom@googlemail.com>
2022-06-10 10:03:33 +0100

    movie git link to footer

--- components/Footer.js
+++ components/Footer.js
@@ -11,7 +11,7 @@ const navigation = [
     return (
       <footer className="bg-white">
         <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-          <nav className="flex flex-wrap justify-center">
+          {/* <nav className="flex flex-wrap justify-center">
             {
               navigation.map((item, i) => (
                 <div key={i} className="px-6 py-2">
@@ -21,7 +21,20 @@ const navigation = [
                 </div>
               ))
             }
-          </nav>
+          </nav> */}
+
+          <div className='max-w-4xl mx-auto md:grid md:grid-cols-3 gap-8 py-4 px-10 border border-[10px] border-gray-50 rounded-sm'>
+            <p className="col-span-2 text-md text-stone-600 pb-6 md:p-0">This is a simple demonstation of an online shop using the wonderful &quot;Shopify store front&quot; API consumed by NextJs and styled with TailwindCSS.</p>
+            <div className="col-span-1 py-4">
+              <a
+                target='_blank' rel='noreferrer'
+                href="https://github.com/PeterBroom/shopify-nextjs-tailwind"
+                className="w-full flex items-center justify-center px-8 py-3 m-0 border border-transparent text-sm md:text-md font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-500"
+              >
+                View on GitHub
+              </a>
+            </div>
+          </div>
           <p className="mt-8 text-center text-gray-400">&copy; 2022 All right reserved.</p>
         </div>
       </footer>
