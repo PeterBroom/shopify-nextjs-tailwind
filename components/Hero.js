bbac7b2c5862a9c14bf60f6f1f6161c48e0c0a79
PeterBroom <harleybroom@googlemail.com>
2022-06-10 10:03:56 +0100

    add hero animation

--- components/Hero.js
+++ components/Hero.js
@@ -1,56 +1,55 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { useEffect } from 'react'
 import Image from 'next/image'
-
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+import { motion, useAnimation } from 'framer-motion'
+import { Logo } from './Logo'
 
 export default function Hero() {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start((i) => ({
+      opacity: [0,1],
+      transition: {
+        duration: 2,
+        delay: i * 0.8
+      }
+    }));
+  }, [controls]);
+
   return (
     <div className="relative bg-white overflow-hidden">
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 after:content-['*'] after:bg-slate-900/70 after:h-full after:w-full after:absolute">
         <Image
-            className="h-full w-full object-cover"
-            src='https://tailwindui.com/img/ecommerce-images/product-feature-02-full-width.jpg'
-            alt="Background image"
-            layout="fill"
+          className="h-full w-full object-cover"
+          src="https://tailwindui.com/img/ecommerce-images/product-feature-02-full-width.jpg"
+          alt="Background image"
+          layout="fill"
         />
       </div>
 
-      <div className="relative pt-6 pb-16 sm:pb-24">
-        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
-          <div className="mb-10 text-center">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 drop-shadow-xl sm:text-5xl md:text-6xl">
-              <span className="block text-white xl:inline">Shopify</span>{' '}
-              <span className="block text-emerald-600 xl:inline">With NextJs and Tailwind</span>
-            </h1>
-          </div>
-
-          <div className="p-10 text-center bg-white/80 max-w-4xl mx-auto">
-            <p className="mt-3 max-w-md mx-auto text-base text-gray-700 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-              This is a simple demonstation of an online shop using the wonderful &quot;Shopify store front&quot; API consumed by NextJs and styled with TailwindCSS.
-            </p>
-            <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-              
-              <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                <a
-                  target='_blank' rel='noreferrer'
-                  href="https://github.com/PeterBroom/shopify-nextjs-tailwind"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-500 md:py-4 md:text-lg md:px-10"
-                >
-                  View on GitHub
-                </a>
-              </div>
+      <div className="relative min-h-[300px] md:min-h-[500px] max-w-screen-xl px-4 mx-auto flex items-center justify-center">
+        <div className='h-full flex items-center justify-center'>
+          <div className='h-full mr-5 md:mr-10'>
+            <Logo />
             </div>
-          </div>
-        </main>
+
+          <h1 className="md:py-10 tracking-wide font-extrabold drop-shadow-xl">
+            <motion.span 
+              className="block text-white text-3xl sm:text-5xl md:text-6xl mb-4"
+              custom={0}
+              animate={controls}
+            >Quill Stationary</motion.span>{" "}
+            <motion.span
+              className="block text-slate-300 text-2xl sm:text-4xl md:text-4xl"
+              custom={1} 
+              animate={controls}
+            >
+              With NextJs and Tailwind
+            </motion.span>
+          </h1>
+        </div>
+
       </div>
     </div>
-  )
+  );
 }
---
+++ components/Logo.js
@@ -0,0 +1,84 @@
+
+  import { useEffect } from 'react'
+  import { motion, useAnimation } from 'framer-motion'
+
+  export const Logo = () => {
+    const controls = useAnimation();
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        controls.start("finished");
+      }, 1500);
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [controls]);
+    
+    const svgVariants = {
+      start: {
+        opacity: 0
+      },
+      finished: {
+        opacity: 1,
+        transition: {
+          duration: 1,
+          ease: 'easeInOut'
+        },
+      }
+    }
+    
+    const pathVariants = {
+      finished: (i) => ({
+        opacity: 1,
+        pathLength: 1,
+        transition: {
+          duration: 0.5,
+          delay: (i = 0 ? 2 : i * 0.2),
+          // ease: 'linear'
+        },
+      }),
+      start: {
+        opacity: 0,
+        pathLength: 0,
+      }
+    }
+
+    const paths = [
+      {
+        d: "M4.83333 53.1667L38.6667 19.3333"
+      },
+      {
+        d: "M21.75 36.25L42.2917 36.25"
+      },
+      {
+        d: "M48.9133 29.58C51.6341 26.8592 53.1626 23.169 53.1626 19.3213C53.1626 15.4735 51.6341 11.7833 48.9133 9.06252C46.1925 6.34173 42.5024 4.8132 38.6546 4.8132C34.8068 4.8132 31.1166 6.34173 28.3958 9.06252L12.0833 25.375V45.9167H32.625L48.9133 29.58Z"
+      }
+    ]
+    
+    return (
+      <div className='w-[80px] h-[80px] md:w-[120px] md:h-[120px]'>
+      <motion.svg
+        viewBox="0 0 58 58"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        variants={svgVariants}
+        initial="start"
+        animate="finished"
+      >
+        {paths.map((item, i) => (
+          <motion.path
+            key={i}
+            d={item.d}
+            initial="start"
+            custom={i}
+            stroke="white"
+            strokeWidth="4.83333"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            animate={controls}
+            variants={pathVariants}
+          />
+        ))}
+      </motion.svg>
+      </div>
+    )
+  }
